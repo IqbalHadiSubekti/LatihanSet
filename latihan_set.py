34ebb2a0b03fcd0792a543a3a11390765ee8f2c9
IqbalHadiSubekti <33026916+IqbalHadiSubekti@users.noreply.github.com>
2021-03-24 23:17:21 +0700

    yes
    ok

---
+++ latihan_set.py
@@ -0,0 +1,9 @@
+frank = ("03 Maret 1990",{"Konsultan Pajak", "Staf Keuangan"}, {"Pengatur Kebijakan"})
+print("Frank: ",frank)
+
+frank[1].add("Kepala Staf Keuangan")
+print("Frank: ",frank)
+
+for i in frank:
+	print(frank[1])
+	print(frank[2])
